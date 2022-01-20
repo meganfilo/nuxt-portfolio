85f57805d143872c0f7b3c8d23914fb893063038
Megan Filo <mfilo@gitlab.com>
2022-01-19 19:46:54 -0600

    Add navigation

--- components/layout/Navigation.vue
+++ components/layout/Navigation.vue
@@ -1,9 +1,15 @@
 <template>
   <header>
-
-      <nav>
-          <nuxt-link to="/">Home</nuxt-link>
-      </nav>
+      <Container>
+        <nav>
+            <NuxtLink to="/" exact class="brand">Megan<span>Filo</span></NuxtLink>
+            <!-- <div class="nav-links">
+              <NuxtLink to="/about">About</NuxtLink>
+              <NuxtLink to="/articles">Articles</NuxtLink>
+              <div class="mode-switch">☀️</div>
+            </div> -->
+        </nav>
+      </Container>
   </header>
 </template>
 
@@ -13,7 +19,36 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 @import '~assets/styles/_variables.scss';
+nav {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
 
+  a, a:hover, .nuxt-link, .nuxt-link-active {
+      text-decoration: none;
+      color: $text-color;
+  }
+
+  a.brand {
+    font-size: 2em;
+    color: $text-color;
+
+    span {
+      color: $header-color;
+      font-weight: bold;
+    }
+  }
+
+  .nav-links {
+    display: flex;
+    justify-content: flex-end;
+    font-size: 1.15em;
+
+    a, div {
+      margin-left: 3rem;
+    }
+  }
+}
 </style>
