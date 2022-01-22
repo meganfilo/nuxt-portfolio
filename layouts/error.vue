8eecff7874f86aa948251dff976d010010bea39c
Megan Filo <mfilo@gitlab.com>
2022-01-22 10:34:04 -0600

    Add 404 page

---
+++ layouts/error.vue
@@ -0,0 +1,18 @@
+<template>
+    <main class="error-page">
+        <Container>
+            <h1>404</h1>
+            <p>You wound up on a page that doesn't exist.</p>
+        </Container>
+    </main>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+.error-page {
+    text-align: center;
+}
+</style>
