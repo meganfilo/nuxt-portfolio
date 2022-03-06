4754abb1af63c31d74a8a20938307394416b3064
Megan Filo <mfilo@gitlab.com>
2022-03-06 09:44:11 -0600

    add google analytics

--- nuxt.config.js
+++ nuxt.config.js
@@ -26,7 +26,10 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [{
+      src: '@/plugins/vue-gtag.js',
+      mode: 'client'
+    }],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: [
